6d6a8306053e297023786a0fc06c8ac559af4924
adair <s22120197@alumnos.itsur.edu.mx>
2026-05-23 20:17:44 -0600

    correcion program

--- Program.cs
+++ Program.cs
@@ -4,8 +4,20 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+// ----- 1. REGISTRAR EL SERVICIO CORS (Justo aquí) -----
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("PermitirReact", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+// -----------------------------------------------------
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -25,6 +37,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// ----- 2. ACTIVAR CORS (Debe ir exactamente ARRIBA de Authorization) -----
+app.UseCors("PermitirReact");
+// ------------------------------------------------------------------------
 
 app.UseAuthorization();
 
